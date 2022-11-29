edfb88f855120d47609f5d804d670b43180b309f
abdallahifox <abdallahiifox@gamil.com>
2022-11-29 17:04:37 +0200

    fix: product state

--- src/Pages/ProductID.jsx
+++ src/Pages/ProductID.jsx
@@ -5,7 +5,7 @@ import { getProduct } from "../utilites/getProducts";
 import ProductsContext from "../Context/products-context";
 
 function ProductID() {
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState(null);
 
   const { addToItems } = useContext(ProductsContext);
 
@@ -15,23 +15,23 @@ function ProductID() {
 
   //route
   let { id } = useParams();
-
   useEffect(() => {
     getProduct(id).then((product) => setProduct(product));
     document.title = `Product Details - Shopping-Cart`;
   }, []);
   return (
     <div className="bg-slate-800 grid md:grid-cols-2 gap-4 p-4 fade-in">
-      <>
-        <div className="text-center">
-          <div className="flex flex-col items-center  justify-center">
-            <h1 className="text-md md:text-xl font-bold">{product.title}</h1>
-            <div className="w-32 my-4">
-              <img src={product.images[0]} alt={product.title} />
+      {product && (
+        <>
+          <div className="text-center">
+            <div className="flex flex-col items-center  justify-center">
+              <h1 className="text-md md:text-xl font-bold">{product.title}</h1>
+              <div className="w-32 my-4">
+                <img src={product.images[0]} alt={product.title} />
+              </div>
             </div>
           </div>
-        </div>
-        {product && (
+
           <div className="flex flex-col h-full text-center md:text-left items-center justify-center">
             <p>{product.description}</p>
             <div className="flex items-center justify-center md:justify-start md:items-start w-full mt-3 gap-3">
@@ -49,8 +49,8 @@ function ProductID() {
               </Link>
             </div>
           </div>
-        )}
-      </>
+        </>
+      )}
     </div>
   );
 }
